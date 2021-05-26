026e579418f25c355f67b80272c04a43770a639a
Himanshu <himanshusearchhomeindia@gmail.com>
2021-05-26 11:12:58 +0530

    adding style into the elements

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
+// below we have imported the css file for stylig the component.
+import './ExpenseItem.css';
+
 // This is our first custom component.
 function ExpenseItem() {
     return(
         // In react function based components we cannot have multiple root elements, so for that we will create one element and inside that element we store all the other element. 
-        <div>
+        <div className="expense-item">
             <div>March 28th 2021</div>
-            <div>
-                <h1>Car Insurance</h1>
-                <div>$294.67</div>
+            <div className="expense-item__description">
+                <h2>Car Insurance</h2>
+                <div className="expense-item__price">$294.67</div>
             </div>
         </div>
     )
