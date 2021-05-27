69e38087fa9588343333c944f756c649e53da572
Himanshu <himanshusearchhomeindia@gmail.com>
2021-05-27 10:55:44 +0530

    outputting dynamic data & working with expression in jsx

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,13 +3,18 @@ import './ExpenseItem.css';
 
 // This is our first custom component.
 function ExpenseItem() {
+    // here we will pass the values from a variable into the below jsx code.
+    const name = 'Cell Phone';
+    const date = new Date(2021, 4, 27);
+    const price = '1800';
+    // for passig the variable values into the jsx we use {} inside this we can pass the variable values.
     return(
         // In react function based components we cannot have multiple root elements, so for that we will create one element and inside that element we store all the other element. 
         <div className="expense-item">
-            <div>March 28th 2021</div>
+            <div>{date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$294.67</div>
+                <h2>{name}</h2>
+                <div className="expense-item__price">${price}</div>
             </div>
         </div>
     )
