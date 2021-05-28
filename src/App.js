bc85395df03921673f3ed6fc2125f4e0191ab29e
Himanshu <himanshusearchhomeindia@gmail.com>
2021-05-28 10:47:22 +0530

    Assignment no.1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // This is the file which get executed whenever the react project gets loaded into the browser, and in this file we will import all the other components.
-// below we will import the FirstComponent that we have created in ExpenseItem.js
-import ExpenseItem from './components/ExpenseItem';
+// below we are importing the component which we are going to use to show the expenses.
+import Expenses from './components/Expenses';
 function App() {
   // now we will use props(properties) to send the data into our components.
   // object for sending the values.
@@ -30,14 +30,8 @@ function App() {
   return (
     <div>
       <h2>Hey boy you can do it.</h2>
-      {/* below we have used the imported component and its a rule in react the everytime we will write the component first letter in capital */}
-      
-      {/* below we will pass the values of the objects in the form of attributes into the below component by using {} */}
-      <ExpenseItem name={expenses[0].name} price={expenses[0].price} date={expenses[0].date}></ExpenseItem>
-      <ExpenseItem name={expenses[1].name} price={expenses[1].price} date={expenses[1].date}></ExpenseItem>
-      <ExpenseItem name={expenses[2].name} price={expenses[2].price} date={expenses[2].date}></ExpenseItem>
-      <ExpenseItem name={expenses[3].name} price={expenses[3].price} date={expenses[3].date}></ExpenseItem>
-      {/* The above data is sent into the component in the form of the props */}
+      {/* below we are sending the expenses object to the Expenses component */}
+      <Expenses item={expenses}/>
     </div>
   );
 }
---
+++ src/components/Expenses.js
@@ -0,0 +1,22 @@
+// Import ExpenseItem Component.
+import ExpenseItem from './ExpenseItem';
+// import the Expenses.css
+import './Expenses.css'
+
+function Expenses(props){
+    // This component will show the expenses into the page.
+    return(
+        <div className="expenses">
+            {/* below we have used the imported component and its a rule in react the everytime we will write the component first letter in capital */}
+            
+            {/* below we will pass the values of the objects in the form of attributes into the below component by using {} */}
+            <ExpenseItem name={props.item[0].name} price={props.item[0].price} date={props.item[0].date} />
+            <ExpenseItem name={props.item[1].name} price={props.item[1].price} date={props.item[1].date} />
+            <ExpenseItem name={props.item[2].name} price={props.item[2].price} date={props.item[2].date} />
+            <ExpenseItem name={props.item[3].name} price={props.item[3].price} date={props.item[3].date} />
+            {/* The above data is sent into the component in the form of the props */}
+        </div>
+    )
+}
+
+export default Expenses;
