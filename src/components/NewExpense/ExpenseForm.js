310dc6abc970e633a3dacd86baba87c6e5d0348a
Himanshu <himanshusearchhomeindia@gmail.com>
2021-06-06 21:19:12 +0530

    Handling form Submission and adding two way binding in form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -42,25 +42,41 @@ const ExpenseForm = () => {
         
     };
     // console.log([userInput.enteredAmount, userInput.enteredDate, userInput.enteredTitle]);
-    console.log([enteredAmount, enteredDate, enteredTitle]);
+    
+    // This function will handle the submit event of form.  
+    const submitHandler = (event) => {
+        event.preventDefault(); //this function prevent the default behaviour of form, i.e page reloading.
+        // here we will store the data in an object.
+        const enteredData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate),
+        };
+        console.log(enteredData); //testing
+
+        // here we are using two way binding for storing the data into the object at the time of form submittion and then below we will reset the form as well.
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    };
     return(
         // This form will get render into the App.js to add new expenses.
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label htmlFor="title">Title</label>
                     {/* below we've added the event listner and whenever the event occurs this will call the handler */}
-                    <input type="text" onChange={titleChangeHandler} />
+                    <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label htmlFor="amount">Amount</label>
                     {/* below we've added the event listner and whenever the event occurs this will call the handler */}
-                    <input type="number" min='0.01' step='0.01' onChange={amountChangeHandler}/>
+                    <input type="number" min='0.01' step='0.01' value={enteredAmount} onChange={amountChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label htmlFor="date">Date</label>
                     {/* below we've added the event listner and whenever the event occurs this will call the handler */}
-                    <input type="date" min='2019-01-01' max='2020-12-31' onChange={dateChangeHandler}/>
+                    <input type="date" min='2019-01-01' max='2020-12-31' value={enteredDate} onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div className="new-expense__actions">
