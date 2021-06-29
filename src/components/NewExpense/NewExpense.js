e3afe30339e4b9ed8bfac97761644d39e60265cd
Himanshu <himanshusearchhomeindia@gmail.com>
2021-06-29 10:40:48 +0530

    Assingment4

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,13 @@
+//import user state
+import { useState } from 'react';
 // importing the style into this component.
 import './NewExpense.css'
 // importing the form component to use it here.
 import ExpenseForm from './ExpenseForm';
 // this function will render the NewExpense component into the page.
 const NewExpense = (props) => {
+    // below we are setting the state to false for using it into the add new expense button.
+    const [isEditing, setIsEditing] = useState(false);
     // here we are using the props to access the function that is coming in the form of props from the App.js.
     const saveExpenseDataHandler = (enteredExpenseData) => {
         // This function will get passed inside the ExpensForm component in the form of props to get the new expense data.
@@ -14,12 +18,23 @@ const NewExpense = (props) => {
         };
         // here we are sending the expenseData into App.js by using the function that has been created into the App.js. 
         props.onAddExpense(expenseData);
+        // this will hide the form and show's the add new expense button.
+        setIsEditing(false);
     }
 
+    // below function will show the form when user clicks on add new expense button.
+    const startEditingHandler = () =>{
+        setIsEditing(true);
+    }
+    // below function will hide the form when user clicks on add new expense button.
+    const stopEditingHandler = () =>{
+        setIsEditing(false);
+    }
     return(
         <div className="new-expense">
+            {!isEditing && <button onClick={startEditingHandler}> Add New Expense</button>}
             {/* below we are sending the saveExpenseDataHandler function in the form of props to get the newExpense data. */}
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancle={stopEditingHandler}/>}
         </div>
     )
 };
